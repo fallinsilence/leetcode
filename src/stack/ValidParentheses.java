7eefdd0f17f73e21e842da9768c331da55ce3ad3
fallinsilence <2057035328@qq.com>
2018-06-22 19:28:04 +0800

    first commit

---
+++ src/stack/ValidParentheses.java
@@ -0,0 +1,40 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        boolean valid = new ValidParentheses().isValid("[]{}");
+        System.out.println(valid);
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> leftBracket = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{'){
+                leftBracket.push(ch);
+            } else {
+                if (leftBracket.size() == 0)
+                    return false;
+
+                switch (ch){
+                    case ')' :
+                        if (leftBracket.pop() != '(')
+                            return false;
+                        break;
+                    case ']' :
+                        if (leftBracket.pop() != '[')
+                            return false;
+                        break;
+                    case '}' :
+                        if (leftBracket.pop() != '{')
+                            return false;
+                        break;
+                }
+            }
+        }
+        return leftBracket.size() == 0;
+    }
+}
